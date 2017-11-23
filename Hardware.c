2c01ee5df02268017a238e57006aed32a2aaabb2
Alex Petkovic <ajetkov@gmail.com>
2017-11-23 18:47:55 -0500

    Init

---
+++ Hardware.c
@@ -0,0 +1,179 @@
+#include "EV3Servo-lib-UW.c"
+
+/*****************************
+ ********** HOPPER ***********
+ *****************************/
+
+const int HOP_TIMER1 = T1, HOP_TIMER2 = T2, HOP_PORT = 0;
+const int HOP_SPEED = 25, HOP_REVERSE_SPEED = 75, HOP_REVERSE_TIME = 250, HOP_REVERSE_INTERVAL = 5000;
+
+void runHopper(int time)
+{
+
+	time1[HOP_TIMER1] = 0;
+	time1[HOP_TIMER2] = 0;
+
+	while(time1[HOP_TIMER1] < time)
+	{
+		motor[HOP_PORT] = -HOP_SPEED;
+
+		while (time1[HOP_TIMER2] < HOP_REVERSE_INTERVAL)
+		{
+			nMotorEncoder[HOP_PORT] = 0;
+			delay(250);
+			displayBigTextLine(0, "%d", nMotorEncoder[HOP_PORT]);
+			if (nMotorEncoder[HOP_PORT] > -360 / HOP_SPEED)
+			{
+				motor[HOP_PORT] = HOP_REVERSE_SPEED;
+				delay(HOP_REVERSE_TIME);
+				motor[HOP_PORT] = -HOP_SPEED;
+			}
+		}
+
+		time1[HOP_TIMER2] = 0;
+
+		motor[HOP_PORT] = HOP_REVERSE_SPEED;
+		delay(HOP_REVERSE_TIME);
+	}
+
+}
+
+/*****************************
+ ****** COIN DISPENSER *******
+ *****************************/
+
+const int CLR_SENS_PT = S4, CLR_SENS_THRESHOLD = 20;
+
+bool coinDispensed(int time)
+{
+	bool coinDetected = false;
+
+	time1[T2] = 0;
+	while(time1[T2] < time)
+	{
+		//displayBigTextLine(0, "Refl: %d", SensorValue[CLR_SENS_PT]);
+		if (SensorValue[CLR_SENS_PT] > CLR_SENS_THRESHOLD)
+			coinDetected = true;
+		delay(25);
+	}
+
+	return coinDetected;
+}
+
+const int SERVO_CTRLR_PT = S1;
+
+const int DIME_MTR_PT = motorB, DIME_MTR_SPD = 10, DIME_DISPENSE_TIME = 5000;
+
+const int NICKL_QRTR_SVO_PT = 1, NICKL_QRTR_OUT_POS = 5, NICKL_POS = 70, QRTR_POS = -58, ;
+const int LNIE_TNIE_SVO_PT = 2, LNIE_TNIE_OUT_POS = 24, LNIE_POS = -46, TNIE_POS = 95;
+const int SERVO_GETCOIN_DELAY = 600; // ms
+
+const int COIN_SENSE_TIME = 1000;
+
+void homeServos()
+{
+	setServoPosition(SERVO_CTRLR_PT, NICKL_QRTR_SVO_PT, NICKL_QRTR_OUT_POS);
+	setServoPosition(SERVO_CTRLR_PT, LNIE_TNIE_SVO_PT, LNIE_TNIE_OUT_POS);
+	delay(SERVO_GETCOIN_DELAY);
+}
+
+int getCoins(int coinType, int number)
+{
+	bool dispensed = true;
+	int count = 0;
+
+	switch (coinType)
+	{
+		// Nickels
+		case 0:
+
+			while (count < number && dispensed)
+			{
+				setServoPosition(SERVO_CTRLR_PT, NICKL_QRTR_SVO_PT, NICKL_POS);
+				delay(SERVO_GETCOIN_DELAY);
+				setServoPosition(SERVO_CTRLR_PT, NICKL_QRTR_SVO_PT, NICKL_QRTR_OUT_POS);
+				dispensed = coinDispensed(COIN_SENSE_TIME);
+				if (dispensed)
+					count++;
+			}
+			break;
+
+		// Dimes
+		case 1:
+
+			while (count < number && dispensed)
+			{
+				motor[DIME_MTR_PT] = DIME_MTR_SPD;
+
+				nMotorEncoder[DIME_MTR_PT] = 0;
+
+			  dispensed = false;
+
+				time1[T3] = 0;
+				while(time1[T3] < DIME_DISPENSE_TIME && !dispensed)
+				{
+					displayBigTextLine(0, "Refl: %d Time: %d", SensorValue[CLR_SENS_PT], time1[T3] / 1000);
+					if (SensorValue[CLR_SENS_PT] > CLR_SENS_THRESHOLD)
+						dispensed = true;
+					delay(25);
+
+					if (nMotorEncoder[DIME_MTR_PT] > 400)
+						motor[DIME_MTR_PT] = 0;
+				}
+
+				motor[DIME_MTR_PT] = 0;
+
+				if (dispensed)
+					count++;
+			}
+			break;
+
+		// Quarters
+		case 2:
+
+			while (count < number && dispensed)
+			{
+				setServoPosition(SERVO_CTRLR_PT, NICKL_QRTR_SVO_PT, QRTR_POS);
+				delay(SERVO_GETCOIN_DELAY);
+				setServoPosition(SERVO_CTRLR_PT, NICKL_QRTR_SVO_PT, NICKL_QRTR_OUT_POS);
+				dispensed = coinDispensed(COIN_SENSE_TIME);
+				if (dispensed)
+					count++;
+			}
+			break;
+
+		// Loonies
+		case 3:
+
+			while (count < number && dispensed)
+			{
+				setServoPosition(SERVO_CTRLR_PT, LNIE_TNIE_SVO_PT, LNIE_POS);
+				delay(SERVO_GETCOIN_DELAY);
+				setServoPosition(SERVO_CTRLR_PT, LNIE_TNIE_SVO_PT, LNIE_TNIE_OUT_POS);
+				dispensed = coinDispensed(COIN_SENSE_TIME);
+				if (dispensed)
+					count++;
+			}
+			break;
+
+		// Toonies
+		case 4:
+
+			while (count < number && dispensed)
+			{
+				setServoPosition(SERVO_CTRLR_PT, LNIE_TNIE_SVO_PT, TNIE_POS);
+				delay(SERVO_GETCOIN_DELAY);
+				setServoPosition(SERVO_CTRLR_PT, LNIE_TNIE_SVO_PT, LNIE_TNIE_OUT_POS);
+				dispensed = coinDispensed(COIN_SENSE_TIME);
+				if (dispensed)
+					count++;
+			}
+			break;
+
+		default:
+			return -1;
+			break;
+	}
+
+	return count;
+}
---
+++ Interface.c
@@ -0,0 +1,120 @@
+/*****************************
+ ******** USER INPUT *********
+ *****************************/
+
+void getInputAmounts(int & dollars, int & cents)
+{
+  displayBigTextLine(0, "Dispense: ");
+	displayBigTextLine(8,  "    ^");
+	displayBigTextLine(9, "  $%02d.%02d", 0, 0);
+	displayBigTextLine(11, "    v");
+
+	string coinType[5] = {"Nickel", "Dime", "Quarter", "Loonie", "Toonie"};
+	short placeHolder = 1;
+
+	while (getButtonPress(buttonBack) == 0)
+	{
+		while (getButtonPress(buttonEnter) == 0)
+		{
+      while (getButtonPress(buttonAny) == 0) {}
+
+      if (getButtonPress(buttonRight))
+      {
+        while (getButtonPress(buttonRight)) {}
+        placeHolder = 0;
+        displayBigTextLine(8,  "       ^");
+        displayBigTextLine(11, "       v");
+      }
+      else if (getButtonPress(buttonLeft))
+      {
+        while (getButtonPress(buttonLeft)) {}
+        placeHolder = 1;
+        displayBigTextLine(8,  "    ^");
+        displayBigTextLine(11, "    v");
+      }
+      else if (getButtonPress(buttonUp))
+      {
+        while (getButtonPress(buttonUp)) {}
+        if (placeHolder == 0 && cents < 100)
+          cents += 5;
+        if (placeHolder == 1 && dollars < 100)
+          dollars++;
+      }
+      else if (getButtonPress(buttonDown))
+      {
+        while (getButtonPress(buttonDown)) {}
+        if (placeHolder == 0 && cents > 0)
+          cents -= 5;
+        if (placeHolder == 1 && dollars > 0)
+          dollars--;
+      }
+
+      displayBigTextLine(9, "  $%02d.%02d", dollars, cents);
+
+      delay(100);
+		}
+
+		displayBigTextLine(9, "  $%02d.%02d", 0, 0);
+
+	}
+
+}
+
+/*****************************
+ ******** MONEY MATH *********
+ *****************************/
+
+float round (float fnum)
+{
+	float fTemp = fnum;
+	const float TOL = 0.001;
+
+	if (fmod (fnum, 0.05) > TOL)
+		fTemp = fnum - fmod(fnum, 0.05) + 0.05;
+
+	return fTemp;
+}
+
+void coinsToDispense (float change, int coins[], bool coinsAvailable[])
+{
+	change = round(change);
+
+	if (change >= 2 && coinsAvailable[TOONIES])
+	{
+		coins[TOONIES] = int (change / 2);
+
+		change -= coins[TOONIES] * 2;
+		change = round(change);
+	}
+
+	if (change >= 1 && coinsAvailable[LOONIES])
+	{
+		coins[LOONIES] = int (change);
+
+		change -= coins[LOONIES];
+		change = round(change);
+	}
+
+	if (change >= 0.25 && coinsAvailable[QUARTERS])
+	{
+		coins[QUARTERS] = int (change / 0.25);
+
+		change -= coins[QUARTERS] * 0.25;
+		change = round(change);
+	}
+
+	if (change >= 0.1 && coinsAvailable[DIMES]);
+	{
+		coins[DIMES] = int (change / 0.1);
+
+		change -= coins[DIMES] * 0.1;
+		change = round(change);
+	}
+
+	if (change >= 0.05 && coinsAvailable[NICKELS])
+	{
+		coins[NICKELS] = int (change / 0.05);
+
+		change -= coins[NICKELS] * 0.05;
+	}
+}
